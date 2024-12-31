8ecbd63334b34a78cde7359d2c315ee001be1190
panhaoyu <panhaoyu.china@outlook.com>
2024-12-31 14:26:06 +0800

    将处理过程拆分为多个文件

--- scripts/p1_读取点云数据.py
+++ scripts/p1_读取点云数据.py
@@ -8,7 +8,6 @@ import open3d
 from PIL import Image
 from open3d.cpu.pybind.geometry import PointCloud
 from sci_cache import MethodDiskCache, sci_method_cache
-from scipy.interpolate import griddata
 
 from scripts.config import base_dir, project_name
 
@@ -73,80 +72,6 @@ class PointCloudProcessor(MethodDiskCache):
         plt.colorbar(label='Density')
         plt.tight_layout()
         plt.show()
-
-    @property
-    @sci_method_cache
-    def p8_表面二维重建(self) -> np.ndarray:
-        """
-        将上表面的点云通过散点插值转换为 x, y 平面内的 [z, r, g, b] 四层矩阵。
-
-        Args:
-            resolution: (x_res, y_res)，表示插值后矩阵的大小，即 x 和 y 方向的分辨率。
-
-        Returns:
-            np.ndarray: 生成的矩阵，包含 [z, r, g, b] 四个层。
-        """
-        cloud = self.p6_仅保留顶面
-
-        resolution: float = 0.1
-
-        points = np.asarray(cloud.points)
-
-        # 提取 x, y, z, r, g, b 数据
-        x, y, z = points.T
-
-
-        x_min = np.min(x)
-        x_max = np.max(x)
-        y_min = np.min(y)
-        y_max = np.max(y)
-        x_count = int((x_max - x_min) / resolution)
-        y_count = int((y_max - y_min) / resolution)
-
-        # 创建网格
-        x_edge = np.linspace(x_min, x_max, x_count)
-        y_edge = np.linspace(y_min, y_max, y_count)
-        x_grid, y_grid = np.meshgrid(x_edge, y_edge)
-
-        # 插值 z, r, g, b 数据
-        arrays = []
-        z_interp = griddata((x, y), z, (x_grid, y_grid), method='cubic')
-        arrays.extend([z_interp])
-        colors = np.asarray(cloud.colors)
-        if colors.size:
-            r, g, b = colors.T
-            r_interp = griddata((x, y), r, (x_grid, y_grid), method='cubic')
-            g_interp = griddata((x, y), g, (x_grid, y_grid), method='cubic')
-            b_interp = griddata((x, y), b, (x_grid, y_grid), method='cubic')
-            arrays.extend([r_interp, g_interp, b_interp])
-
-        # 生成 [z, r, g, b] 四层矩阵
-        interpolated_matrix = np.stack(arrays, axis=-1)
-
-        return interpolated_matrix
-
-    def 绘制表面(self):
-        interpolated_matrix = self.p8_表面二维重建
-
-        # 绘制高程图
-        elevation = interpolated_matrix[:, :, 0]
-        norm = plt.Normalize(vmin=np.nanmin(elevation), vmax=np.nanmax(elevation))
-        scalar_map = cm.ScalarMappable(cmap='jet', norm=norm)
-        elevation = (scalar_map.to_rgba(elevation)[:, :, :3] * 255)
-        elevation = np.uint8(elevation)
-        Image.fromarray(elevation).save(self.ply_file.with_name('elevation.png'))
-
-        if interpolated_matrix.shape[2] > 1:
-            color = interpolated_matrix[:, :, 1:]
-            for i in range(3):
-                v = color[:, :, i]
-                v_min = np.nanmin(v)
-                v_max = np.nanmax(v)
-                v = (v - v_min) / (v_max - v_min) * 255
-                color[:, :, i] = v
-
-            color = np.uint8(np.round(color))
-            Image.fromarray(color).save(self.ply_file.with_name('surface.png'))
 
 
 
--- scripts/s24123101_高程与颜色的相关性.py
+++ scripts/s24123101_高程与颜色的相关性.py
@@ -4,7 +4,7 @@ from scripts.config import base_dir, project_name
 from scripts.p1_读取点云数据 import PointCloudProcessor
 
 np.set_printoptions(linewidth=400, precision=3, edgeitems=6)
-array = PointCloudProcessor(base_dir, project_name).p8_表面二维重建
+array = PointCloudProcessor(base_dir, project_name).p7_表面二维重建
 
 slicing = 1
 # array = array[slicing:-slicing, slicing:-slicing, :]
