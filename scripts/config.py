1b03df52a8903b2fd5c4c7c854215bfd05041837
panhaoyu <panhaoyu.china@outlook.com>
2025-01-04 14:30:14 +0800

    调整依赖

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 base_dir = Path(r'F:\data\laser-scanner')
-# project_name = 'test specimen A'
-project_name = 'test specimen B'
-# project_name = 'test specimen B side first'
+specimen_name = 'test specimen'
+project_direction = 'B'
+project_name = f'{specimen_name} {project_direction}'
