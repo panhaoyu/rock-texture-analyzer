9045ea505d392a86d8e0faf5d002c6695657f2b6
panhaoyu <panhaoyu.china@outlook.com>
2025-01-04 17:09:13 +0800

    尝试简化提取方式

--- p3_表面显微镜扫描数据处理/p1_建立一个新的处理步骤.py
+++ p3_表面显微镜扫描数据处理/p1_建立一个新的处理步骤.py
@@ -6,9 +6,9 @@ from PIL import Image
 class Processor:
     base_dir = Path(r'F:\data\laser-scanner\others\侧面光学扫描的预处理')
     s1_name = r'1-原始数据'
-    s2_name = r'2-裁剪'
+    s2_name = r'2-转换为PNG'
 
-    def s1_将jpg格式转换为png格式(self):
+    def s2_将jpg格式转换为png格式(self):
         output_dir = self.base_dir / self.s2_name
         for file in self.base_dir.joinpath(self.s1_name).glob('*.jpg'):
             output_file = output_dir.joinpath(f'{file.stem}.png')
@@ -18,7 +18,7 @@ class Processor:
     @classmethod
     def main(cls):
         obj = cls()
-        obj.s1_将jpg格式转换为png格式()
+        obj.s2_将jpg格式转换为png格式()
 
 
 if __name__ == '__main__':
