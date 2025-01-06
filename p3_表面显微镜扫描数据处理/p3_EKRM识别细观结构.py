690b5cdd6f9ff742991aa248f4edc024ce7e0e14
panhaoyu <panhaoyu.china@outlook.com>
2025-01-06 20:53:30 +0800

    识别细观结构

--- p3_表面显微镜扫描数据处理/p3_EKRM识别细观结构.py
+++ p3_表面显微镜扫描数据处理/p3_EKRM识别细观结构.py
@@ -2,7 +2,7 @@ from rock_grain_identifier import RockGrainIdentifier
 from rock_grain_identifier.group import RgiGroup
 
 
-class Identifier(RockGrainIdentifier):
+class GraniteIdentifier(RockGrainIdentifier):
     groups = [
         RgiGroup(name='other', index=1, color='#00FF00'),
         RgiGroup(name='feldspar', index=2, color='#FF0000'),
@@ -10,4 +10,5 @@ class Identifier(RockGrainIdentifier):
         RgiGroup(name='biotite', index=4, color='#000000'),
     ]
 
-identifier = Identifier()
+
+identifier = GraniteIdentifier()
