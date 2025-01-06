034b9bcdd5241d7fa9a931b45d93eb0eea5a8a65
panhaoyu <panhaoyu.china@outlook.com>
2025-01-06 14:32:18 +0800

    补全图像

--- p3_表面显微镜扫描数据处理/config.py
+++ p3_表面显微镜扫描数据处理/config.py
@@ -1,3 +1,6 @@
 raw_image_url = 'https://zhaogroup.oss-cn-shanghai.aliyuncs.com/temp/18-需要补全的区域/G-D-1-1.png'
 mask_image_url = 'https://zhaogroup.oss-cn-shanghai.aliyuncs.com/temp/19-识别黑色水平线区域/G-D-1-1.png'
 keep_image_url = 'https://zhaogroup.oss-cn-shanghai.aliyuncs.com/temp/20-翻转黑白区域/G-D-1-1.png'
+
+bucket_name = "zhaogroup-hk"
+endpoint = 'oss-cn-hongkong.aliyuncs.com'
--- p3_表面显微镜扫描数据处理/p2_图像补全_阿里云.py
+++ p3_表面显微镜扫描数据处理/p2_图像补全_阿里云.py
@@ -1,7 +1,3 @@
-"""
-尝试调用阿里的接口，不过失败，无法使用。
-"""
-
 import os
 import time
 
@@ -50,3 +46,36 @@ def erase_image(image_url: str, mask_url: str, foreground_url: str) -> str:
                 raise Exception(f"任务执行失败: {message}")
             case _:
                 time.sleep(5)
+
+
+from pathlib import Path
+import subprocess
+
+from p3_表面显微镜扫描数据处理.config import endpoint, bucket_name
+
+
+def upload_to_oss(base_dir: Path, file_path: Path) -> str:
+    """上传文件到OSS并返回访问路径"""
+    relative_path = file_path.relative_to(base_dir)
+    oss_path = Path("temp") / relative_path
+    cmd = [
+        'ossutil64',
+        f'--endpoint={endpoint}',
+        f'--include="{file_path.name}"',
+        'cp',
+        str(file_path),
+        f'oss://{bucket_name}/{oss_path}'
+    ]
+    subprocess.run(cmd, check=True)
+    return f'https://{bucket_name}.{endpoint}/{oss_path}'
+
+
+def erase_image_with_oss(base_dir: Path,
+                         local_image_path: Path,
+                         local_mask_path: Path,
+                         local_foreground_path: Path) -> str:
+    """上传本地文件到OSS并调用erase_image函数"""
+    image_url = upload_to_oss(base_dir, local_image_path)
+    mask_url = upload_to_oss(base_dir, local_mask_path)
+    foreground_url = upload_to_oss(base_dir, local_foreground_path)
+    return erase_image(image_url, mask_url, foreground_url)
