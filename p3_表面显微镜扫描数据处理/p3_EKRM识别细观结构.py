16a9db767f0313bb018d4d5f256bf9a0e66dccea
panhaoyu <panhaoyu.china@outlook.com>
2025-01-06 20:53:18 +0800

    不再补全黑边

--- p3_表面显微镜扫描数据处理/p3_EKRM识别细观结构.py
+++ p3_表面显微镜扫描数据处理/p3_EKRM识别细观结构.py
@@ -1,12 +1,13 @@
 from rock_grain_identifier import RockGrainIdentifier
+from rock_grain_identifier.group import RgiGroup
 
 
 class Identifier(RockGrainIdentifier):
     groups = [
-        RockGrainIdentifier.Group(name='other', index=1, color='#00FF00'),
-        RockGrainIdentifier.Group(name='feldspar', index=2, color='#FF0000'),
-        RockGrainIdentifier.Group(name='quartz', index=3, color='#E1FF00'),
-        RockGrainIdentifier.Group(name='biotite', index=4, color='#000000'),
+        RgiGroup(name='other', index=1, color='#00FF00'),
+        RgiGroup(name='feldspar', index=2, color='#FF0000'),
+        RgiGroup(name='quartz', index=3, color='#E1FF00'),
+        RgiGroup(name='biotite', index=4, color='#000000'),
     ]
 
 identifier = Identifier()
