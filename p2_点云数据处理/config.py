11bf063b259557e6a16c54e574ca46f6dea771c5
panhaoyu <panhaoyu.china@outlook.com>
2025-02-26 17:10:26 +0800

    add: s25022601_点云数据处理_花岗岩_低曝光度.py

--- p2_点云数据处理/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-base_dir = Path(r'F:\data\laser-scanner\25010201-scanner-test-data\25010901-compare-three')
-specimen_name = 'CP1D1'
-project_direction = 'B'
-
-project_name = f'{specimen_name} {project_direction}'
