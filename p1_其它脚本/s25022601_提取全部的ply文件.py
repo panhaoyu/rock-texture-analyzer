10ba87570cb2d2db3f3c4f41176919b2293391ca
panhaoyu <panhaoyu.china@outlook.com>
2025-02-26 15:19:18 +0800

    将一个批次的文件复制出来

---
+++ p1_其它脚本/s25022601_提取全部的ply文件.py
@@ -0,0 +1,9 @@
+import shutil
+from pathlib import Path
+
+input_dir = Path(r'F:\data\laser-scanner\25022601-劈裂面形貌扫描原始数据\01-劈裂面\立方体-花岗岩\低曝光度')
+output_dir = Path(r'F:\data\laser-scanner\25022602-劈裂面形貌扫描-花岗岩-低曝光度\1-原始数据')
+
+output_dir.mkdir(parents=True, exist_ok=True)
+for file in input_dir.glob('*\*.ply'):
+    shutil.copy(file, output_dir)
