280a368dc81bfe7c93761aa95563b00a2a3a9dcf
panhaoyu <panhaoyu.china@outlook.com>
2025-02-28 23:07:10 +0800

    对代码整体进行优化

--- p2_三维点云处理/s25022601_点云数据处理_花岗岩_低曝光度.py
+++ p2_三维点云处理/s25022601_点云数据处理_花岗岩_低曝光度.py
@@ -126,33 +126,38 @@ class s25022602_劈裂面形貌扫描_花岗岩_低曝光度(BaseProcessor):
     def f13_2_仅保留左侧面(self, output_path: Path):
         cloud = self.f10_精细化对正.read(output_path)
         x0, x1, y0, y1, z0, z1 = self.f12_各个面的坐标.read(output_path)
+        z0, z1 = z0 + (z1 - z0) * 0.1, z1 - (z1 - z0) * 0.2
         R = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]], dtype=np.float64)
         cloud.points = Vector3dVector(np.asarray(cloud.points) @ R)
         return point_cloud_keep_top(cloud, z0, z1, y0, y1, x1, x0)
 
-    enable_multithread = False
 
     @mark_as_ply
     @mark_as_recreate
     def f13_3_仅保留右侧面(self, output_path: Path):
         cloud = self.f10_精细化对正.read(output_path)
         x0, x1, y0, y1, z0, z1 = self.f12_各个面的坐标.read(output_path)
+        z0, z1 = z0 + (z1 - z0) * 0.1, z1 - (z1 - z0) * 0.2
         R = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]], dtype=np.float64)
         cloud.points = Vector3dVector(np.asarray(cloud.points) @ R)
         return point_cloud_keep_top(cloud, z0, z1, y0, y1, x0, x1)
 
     @mark_as_ply
+    @mark_as_recreate
     def f13_4_仅保留前面(self, output_path: Path):
         cloud = self.f10_精细化对正.read(output_path)
         x0, x1, y0, y1, z0, z1 = self.f12_各个面的坐标.read(output_path)
+        z0, z1 = z0 + (z1 - z0) * 0.1, z1 - (z1 - z0) * 0.2
         R = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]], dtype=np.float64)
         cloud.points = Vector3dVector(np.asarray(cloud.points) @ R)
         return point_cloud_keep_top(cloud, x0, x1, z0, z1, y1, y0)
 
     @mark_as_ply
+    @mark_as_recreate
     def f13_5_仅保留后面(self, output_path: Path):
         cloud = self.f10_精细化对正.read(output_path)
         x0, x1, y0, y1, z0, z1 = self.f12_各个面的坐标.read(output_path)
+        z0, z1 = z0 + (z1 - z0) * 0.1, z1 - (z1 - z0) * 0.2
         R = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]], dtype=np.float64)
         cloud.points = Vector3dVector(np.asarray(cloud.points) @ R)
         return point_cloud_keep_top(cloud, x0, x1, z0, z1, y0, y1)
@@ -172,10 +177,12 @@ class s25022602_劈裂面形貌扫描_花岗岩_低曝光度(BaseProcessor):
         return self.f13_3_仅保留右侧面.read(output_path)
 
     @mark_as_png
+    @mark_as_recreate
     def f14_4_绘制前面点云(self, output_path: Path):
         return self.f13_4_仅保留前面.read(output_path)
 
     @mark_as_png
+    @mark_as_recreate
     def f14_5_绘制后面点云(self, output_path: Path):
         return self.f13_5_仅保留后面.read(output_path)
 
