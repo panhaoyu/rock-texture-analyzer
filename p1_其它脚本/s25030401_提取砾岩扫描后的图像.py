c987d3a2c417d2e1e8f3804806218108dead3e79
panhaoyu <panhaoyu.china@outlook.com>
2025-03-04 07:36:48 +0800

    添加 s25030401_提取砾岩扫描后的图像.py

--- p1_其它脚本/s25030401_提取砾岩扫描后的图像.py
+++ p1_其它脚本/s25030401_提取砾岩扫描后的图像.py
@@ -2,18 +2,23 @@ import shutil
 from pathlib import Path
 
 # source_dir = Path(r'F:\data\laser-scanner\25010802-砾岩剪切前的断面光学扫描\99-处理结果')
-source_dir = Path(r'F:\data\laser-scanner\25010801-花岗岩剪切前的断面光学扫描\99-处理结果')
+# source_dir = Path(r'F:\data\laser-scanner\25010801-花岗岩剪切前的断面光学扫描\99-处理结果')
+# source_dir = Path(r'F:\data\laser-scanner\25010601-砾岩的侧面光学扫描的预处理\24-人工补全黑边')
+source_dir = Path(r'F:\data\laser-scanner\25010502-花岗岩的侧面光学扫描的预处理\24-人工补全黑边')
 output_dir = source_dir / 'output'
 
+拆分方向 = False
 
 def main():
     src: Path
     for src in source_dir.glob('*.png'):
         name = src.stem.replace('-', '')
-        name, direction = name[:-1], name[-1]
-        direction_dir = output_dir / direction
-        direction_dir.mkdir(parents=True, exist_ok=True)
-        dst = direction_dir / f'{name}.png'
+        final_output_dir = output_dir
+        if 拆分方向:
+            name, direction = name[:-1], name[-1]
+            final_output_dir = final_output_dir / direction
+        final_output_dir.mkdir(parents=True, exist_ok=True)
+        dst = final_output_dir / f'{name}.png'
         shutil.copy(src, dst)
 
 
