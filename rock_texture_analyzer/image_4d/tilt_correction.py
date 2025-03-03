20133c337a859ab3d812a768112523de8a8c662f
panhaoyu <panhaoyu.china@outlook.com>
2025-03-03 20:54:54 +0800

    识别更匹配的上下面

--- rock_texture_analyzer/image_4d/tilt_correction.py
+++ rock_texture_analyzer/image_4d/tilt_correction.py
@@ -10,10 +10,10 @@ def _tilt_correction_inner(arr1: NDArray, arr2: NDArray) -> tuple[NDArray, NDArr
     A = np.column_stack((X.ravel(), Y.ravel(), np.ones(X.size)))
     b = (layer1 - layer2).ravel()
 
-    # 初始拟合计算残差并筛选最佳50%数据点
+    # 初始拟合计算残差并排除匹配不好的数据点
     coefficients = np.linalg.lstsq(A, b, rcond=None)[0]
     residuals = np.abs(b - A @ coefficients)
-    mask = residuals < np.percentile(residuals, 50)
+    mask = residuals < np.percentile(residuals, 90)
 
     # 选取有效点再进行真正的最小二乘拟合
     coefficients = np.linalg.lstsq(A[mask], b[mask], rcond=None)[0]
