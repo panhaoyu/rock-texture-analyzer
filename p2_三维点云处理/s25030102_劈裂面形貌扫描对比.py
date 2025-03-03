a91418a63aae5bd0a7843ea71b9259cfbb81d173
panhaoyu <panhaoyu.china@outlook.com>
2025-03-03 08:06:40 +0800

    识别更匹配的上下面

--- p2_三维点云处理/s25030102_劈裂面形貌扫描对比.py
+++ p2_三维点云处理/s25030102_劈裂面形貌扫描对比.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
@@ -43,30 +44,30 @@ class s25030102_劈裂面形貌扫描对比(SerialProcess):
         return process(array)
 
     @mark_as_npy
-    def f0202_DB放縮(self):
+    def f0202_DB放缩(self):
         array = self.f0102_原始数据_Db
         return process(array)
 
     @mark_as_npy
-    def f0203_UA放縮(self):
+    def f0203_UA放缩(self):
         array = self.f0103_原始数据_Ua
         return process(array)
 
     @mark_as_npy
-    def f0204_UB放縮(self):
+    def f0204_UB放缩(self):
         array = self.f0104_原始数据_Ub
         return process(array)
 
     @mark_as_png
     def f0301_合并显示(self):
-        elevation_ua = depth_matrix_to_elevation_image(self.f0203_UA放縮)
-        colorful_ua = depth_matrix_to_rgb_image(self.f0203_UA放縮)
-        elevation_ub = depth_matrix_to_elevation_image(self.f0204_UB放縮)
-        colorful_ub = depth_matrix_to_rgb_image(self.f0204_UB放縮)
+        elevation_ua = depth_matrix_to_elevation_image(self.f0203_UA放缩)
+        colorful_ua = depth_matrix_to_rgb_image(self.f0203_UA放缩)
+        elevation_ub = depth_matrix_to_elevation_image(self.f0204_UB放缩)
+        colorful_ub = depth_matrix_to_rgb_image(self.f0204_UB放缩)
         elevation_da = depth_matrix_to_elevation_image(self.f0201_DA放缩)
         colorful_da = depth_matrix_to_rgb_image(self.f0201_DA放缩)
-        elevation_db = depth_matrix_to_elevation_image(self.f0202_DB放縮)
-        colorful_db = depth_matrix_to_rgb_image(self.f0202_DB放縮)
+        elevation_db = depth_matrix_to_elevation_image(self.f0202_DB放缩)
+        colorful_db = depth_matrix_to_rgb_image(self.f0202_DB放缩)
 
         # 组合图像矩阵
         upper_row = [elevation_ua, elevation_ub, colorful_ua, colorful_ub]
@@ -77,6 +78,19 @@ class s25030102_劈裂面形貌扫描对比(SerialProcess):
         [merged.paste(img, (i * w, 0)) for i, img in enumerate(upper_row)]
         [merged.paste(img, (i * w, h)) for i, img in enumerate(lower_row)]
         return merged
+
+    @mark_as_png
+    def f0401_比较各个扫描结果的差异(self):
+        # ua, ub, da, db
+        arrays = self.f0203_UA放缩, self.f0204_UB放缩, self.f0201_DA放缩, self.f0202_DB放缩
+        arrays = [array[:, :, 0] for array in arrays]
+        compare = [[arr1 - arr2 for arr2 in arrays] for arr1 in arrays]
+        compare = [[v - np.mean(v) for v in vs] for vs in compare]
+        array = np.hstack([np.vstack(v) for v in compare])
+        figure, axes = plt.subplots(1, 1, figsize=(12, 12))
+        im = axes.imshow(array)
+        figure.colorbar(im)
+        return figure
 
 
 if __name__ == '__main__':
