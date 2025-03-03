45e64b88f2fd1706591ad520eea31ac818e2ff89
panhaoyu <panhaoyu.china@outlook.com>
2025-03-03 20:15:03 +0800

    识别更匹配的上下面

--- rock_texture_analyzer/image_4d/tilt_correction.py
+++ rock_texture_analyzer/image_4d/tilt_correction.py
@@ -5,21 +5,19 @@ from numpy.typing import NDArray
 def _tilt_correction_inner(arr1: NDArray, arr2: NDArray) -> tuple[NDArray, NDArray]:
     arr1, arr2 = arr1.copy(), arr2.copy()
     layer1, layer2 = arr1[:, :, 0], arr2[:, :, 0]
-    diff = layer1 - layer2
     rows, cols = layer1.shape
     X, Y = np.meshgrid(np.arange(cols), np.arange(rows))
     A = np.column_stack((X.ravel(), Y.ravel(), np.ones(X.size)))
-    b = diff.ravel()
+    b = (layer1 - layer2).ravel()
 
     # 初始拟合计算残差并筛选最佳50%数据点
     coefficients = np.linalg.lstsq(A, b, rcond=None)[0]
     residuals = np.abs(b - A @ coefficients)
-    mask = residuals <= np.percentile(residuals, 50)
+    mask = residuals < np.percentile(residuals, 50)
 
     # 选取有效点再进行真正的最小二乘拟合
     coefficients = np.linalg.lstsq(A[mask], b[mask], rcond=None)[0]
-    plane = (A @ coefficients).reshape(layer1.shape)
-    reverse = plane / 2
+    reverse = (A @ coefficients).reshape(layer1.shape) / 2
     layer1 -= reverse
     layer2 += reverse
     arr1[:, :, 0], arr2[:, :, 0] = layer1, layer2
