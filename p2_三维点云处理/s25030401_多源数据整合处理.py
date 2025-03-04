2a74fd8e1535e71cdefda9a7449bb71ea3aff350
panhaoyu <panhaoyu.china@outlook.com>
2025-03-04 15:29:30 +0800

    add s25030401_多源数据整合处理.py

--- p2_三维点云处理/s25030401_多源数据整合处理.py
+++ p2_三维点云处理/s25030401_多源数据整合处理.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
 
+import cv2
 import numpy as np
-from PIL import Image, ImageCms
+from PIL import ImageCms
 
 from batch_processor import SerialProcess, mark_as_npy, mark_as_png, mark_as_source, mark_as_recreate
 from rock_texture_analyzer.image_4d.fix_nan import remove_nan_borders, fill_nan_values
@@ -67,20 +68,23 @@ class s25030401_多源数据整合处理(SerialProcess):
         delta = np.sqrt(np.sum((lab1 - lab2) ** 2, axis=2))
 
         # 应用容差阈值并执行形态学操作
-        delta = (delta > 5).astype(np.uint8)
-        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        # delta = cv2.dilate(delta, kernel)  # 先膨胀连接断裂区域
-        # delta = cv2.erode(delta, kernel)  # 再腐蚀消除孤立噪点
+        delta = (delta > 20).astype(np.uint8)
+        kernel_size = 15
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+        delta = cv2.dilate(delta, kernel)  # 先膨胀连接断裂区域
+        delta = cv2.erode(delta, kernel)  # 再腐蚀消除孤立噪点
 
         return delta
 
     @mark_as_recreate
     @mark_as_png
     def f0202_剪切前_劈裂面_光学差值_绘图(self):
-        return Image.fromarray(self.f0202_剪切前_劈裂面_光学差值, '1')
+        array = self.f0202_剪切前_劈裂面_光学差值
+        array = np.array([[0, 0, 0], [255, 255, 255]])[array]
+        array = array.astype(np.uint8)
+        return array
 
-    is_debug = True
-    # @mark_as_recreate
+    @mark_as_recreate
     @mark_as_png
     def f0501_展示目前已有数据(self):
         return merge_image_grid([[
@@ -92,11 +96,10 @@ class s25030401_多源数据整合处理(SerialProcess):
             add_label(matrix_to_elevation_image(self.f0103_剪切前_劈裂面_三维扫描_下半部分, 10), '剪切前 下 高程'),
             add_label(matrix_to_rgb_image(self.f0103_剪切前_劈裂面_三维扫描_下半部分), '剪切前 下 纹理'),
             add_label(self.f0105_剪切前_劈裂面_光学扫描_下半部分.resize((1000, 1000)), '剪切前 下 光学'),
-            None,
         ], [
-            add_label(matrix_to_elevation_image(self.f0201_剪切前_劈裂面_高程差值, 2), '剪切前 差值'),
+            add_label(matrix_to_elevation_image(self.f0201_剪切前_劈裂面_高程差值, 2), '剪切前 差值 高程'),
             None,
-            None,
+            add_label(self.f0202_剪切前_劈裂面_光学差值_绘图.resize((1000, 1000)), '剪切前 差值 光学'),
         ]])
 
 
