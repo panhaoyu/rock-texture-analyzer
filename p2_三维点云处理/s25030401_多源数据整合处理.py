d818c4b9a12296ea9068d3ae728ece439245df03
panhaoyu <panhaoyu.china@outlook.com>
2025-03-04 11:08:42 +0800

    add s25030401_多源数据整合处理.py

--- p2_三维点云处理/s25030401_多源数据整合处理.py
+++ p2_三维点云处理/s25030401_多源数据整合处理.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 
 import numpy as np
+from PIL import Image, ImageCms
 
 from batch_processor import SerialProcess, mark_as_npy, mark_as_png, mark_as_source, mark_as_recreate
 from rock_texture_analyzer.image_4d.fix_nan import remove_nan_borders, fill_nan_values
@@ -51,6 +52,34 @@ class s25030401_多源数据整合处理(SerialProcess):
         return v1 - v2
 
     @mark_as_recreate
+    @mark_as_npy
+    def f0202_剪切前_劈裂面_光学差值(self):
+        img1, img2 = self.f0104_剪切前_劈裂面_光学扫描_上半部分, self.f0105_剪切前_劈裂面_光学扫描_下半部分
+        # 转换为LAB颜色空间并计算色差
+        srgb_p, lab_p = ImageCms.createProfile('sRGB'), ImageCms.createProfile('LAB')
+        transform = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, 'RGB', 'LAB')
+        # 执行颜色空间转换
+        lab1 = ImageCms.applyTransform(img1, transform)
+        lab2 = ImageCms.applyTransform(img2, transform)
+        lab1 = np.asarray(lab1)
+        lab2 = np.asarray(lab2)
+
+        delta = np.sqrt(np.sum((lab1 - lab2) ** 2, axis=2))
+
+        # 应用容差阈值并执行形态学操作
+        delta = (delta > 5).astype(np.uint8)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        # delta = cv2.dilate(delta, kernel)  # 先膨胀连接断裂区域
+        # delta = cv2.erode(delta, kernel)  # 再腐蚀消除孤立噪点
+
+        return delta.astype(bool)
+
+    @mark_as_recreate
+    @mark_as_png
+    def f0202_剪切前_劈裂面_光学差值_绘图(self):
+        return Image.fromarray(self.f0202_剪切前_劈裂面_光学差值, '1')
+
+    # @mark_as_recreate
     @mark_as_png
     def f0501_展示目前已有数据(self):
         return merge_image_grid([[
